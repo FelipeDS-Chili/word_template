353630626f7c615b21283ef04e30f86227fe349a
felipe inostroza <falberto.inostroza@gmail.com>
2021-02-28 23:10:03 -0300

    testing

--- setup.py
+++ setup.py
@@ -1,6 +1,7 @@
 from setuptools import find_packages
 from setuptools import setup
 
+
 with open('requirements.txt') as f:
     content = f.readlines()
 requirements = [x.strip() for x in content if 'git+' not in x]
@@ -10,6 +11,7 @@ setup(name='word_template',
       description="Project Description",
       packages=find_packages(),
       test_suite = 'tests',
+      install_requires=requirements
       # include_package_data: to install data from MANIFEST.in
       include_package_data=True,
       scripts=['scripts/word_template-run'],
